c294ffcdd9bdc334f36a2d0bc82e8b21fadbf7f5
Ромашка <bombey77@mail.ru>
2016-10-22 16:29:00 +0300

    SA version

--- app/src/main/java/bombey77/sa34/DBHelper.java
+++ app/src/main/java/bombey77/sa34/DBHelper.java
@@ -6,14 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 2;
-    public static final String DATABASE_NAME = "contactDB";
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "contactDb";
     public static final String TABLE_CONTACTS = "contacts";
 
-    public static final String KEY_ID = "_ID";
+    public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
     public static final String KEY_MAIL = "mail";
-
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,14 +20,16 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
+                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
 
-        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID + "integer primary key," + KEY_NAME + "text," + KEY_MAIL + "text" + ")" );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         db.execSQL("drop table if exists " + TABLE_CONTACTS);
+
         onCreate(db);
+
     }
 }
